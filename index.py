0223436b651debf9830be6cf0717ccc5880b7b5c
Peter Atsaves <atsavespeter21@gmail.com>
2024-10-25 21:09:43 -0500

    2025 updates

--- index.py
+++ index.py
@@ -1,20 +1,19 @@
 #pip install GitPython
 
-teams = ['Atlanta Hawks','Boston Celtics','Brooklyn Nets','Charlotte Hornets','Chicago Bulls',
-         'Cleveland Cavaliers','Dallas Mavericks','Detroit Pistons','Denver Nuggets','Golden State Warriors',
-         'Houston Rockets','Indiana Pacers','Los Angeles Clippers','Los Angeles Lakers','Memphis Grizzlies',
-         'Miami Heat','Minnesota Timberwolves','Milwaukee Bucks','New Orleans Pelicans','New York Knicks',
-         'Oklahoma City Thunder','Orlando Magic','Phoenix Suns','Philadelphia 76ers','Portland Trail Blazers',
-         'Sacramento Kings','San Antonio Spurs','Toronto Raptors','Utah Jazz','Washington Wizards']
+teams = ['Atlanta Hawks','Brooklyn Nets','Los Angeles Lakers','New York Knicks','Oklahoma City Thunder','Utah Jazz',
+'Golden State Warriors','Houston Rockets','Milwaukee Bucks','Minnesota Timberwolves','Orlando Magic','Washington Wizards',
+'Charlotte Hornets','Detroit Pistons','Indiana Pacers','Memphis Grizzlies','Miami Heat','Portland Trail Blazers',
+'Cleveland Cavaliers','Los Angeles Clippers','Phoenix Suns','San Antonio Spurs','Sacramento Kings','Toronto Raptors',
+'Boston Celtics','Chicago Bulls','Dallas Mavericks','Denver Nuggets','New Orleans Pelicans','Philadelphia 76ers']
 
-overs = [42.5,54.5,37.5,31.5,37.5,50.5,43.5,28.5,52.5,47.5,31.5,39.5,45.5,46.5,46.5,44.5,44.5,53.5,
-         44.5,45.5,44.5,37.5,51.5,47.5,27.5,44.5,28.5,36.5,35.5,24.5]
+overs = [36.5,19.5,42.5,53.5,57.5,27.5,43.5,42.5,49.5,51.5,47.5,19.5,31.5,25.5,46.5,46.5,43.5,20.5,
+48.5,35.5,48.5,35.5,46.5,28.5,58.5,28.5,49.5,50.5,45.5,49.5]
 
-over_under = ['Over','Over','Over','Under','Over','Over','Under','Under','Over','Over','Under','Over','Over','Under','Under',
-              'Over','Over','Over','Over','Over','Over','Over','Under','Over','Under','Over','Under','Under','Under','Under']
+over_under = ['Over''Under','Over','Over','Over','Under','Under','Over','Over','Over','Over','Under','Over','Over','Under',
+'Over','Over','Over','Over','Under','Over','Under','Over','Under','Over','Over','Under','Over','Over','Over']
 
-people = ['Josh','Alan','Emil','Alan','Eric','Emil','Peter','Peter','Josh','Emil','Emil','Josh','Peter','Emil','Peter',
-          'Alan','Eric','Eric','Alan','Emil','Josh','Eric','Josh','Peter','Eric','Eric','Peter','Alan','Josh','Alan']
+people = ['Alan','Alan','Alan','Alan','Alan','Alan','Emil','Emil','Emil','Emil','Emil','Emil','Eric','Eric','Eric','Eric',
+'Eric','Eric','Josh','Josh','Josh','Josh','Josh','Josh','Peter','Peter','Peter','Peter','Peter','Peter']
 
 wins = []
 losses = []
